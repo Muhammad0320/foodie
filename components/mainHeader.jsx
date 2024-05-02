99c5ac0ebae3ac8fc41af16833fa6fe133688c3c
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-02 05:46:03 +0100

    chore: update

--- components/mainHeader.jsx
+++ components/mainHeader.jsx
@@ -1,15 +1,15 @@
 import Link from "next/link";
-
+import classes from "./mainHeader.module.css";
 import logo from "@/assets/logo.png";
 
 function MainHeader() {
   return (
-    <header>
-      <Link href="/">
+    <header className={classes.header}>
+      <Link href="/" className={classes.logo}>
         <img src={logo.src} alt="A plate w/ food init" />
       </Link>
 
-      <nav>
+      <nav className={classes.nav}>
         <ul>
           <li>
             <Link href="/community"> Meals community </Link>
