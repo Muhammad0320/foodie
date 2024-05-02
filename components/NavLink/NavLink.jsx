a188345e06d244cfd15d5913b3c8c28ab964092e
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-02 07:00:41 +0100

    chore:add navlink

---
+++ components/NavLink/NavLink.jsx
@@ -0,0 +1,11 @@
+"use client";
+import Link from "next/link";
+import { useParams } from "next/navigation";
+
+function NavLink({ children, to }) {
+  const params = useParams();
+
+  return <Link></Link>;
+}
+
+export default NavLink;
