5e8f7807a40d250b2a09702057e0e2b9ab409e29
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-02 06:36:33 +0100

    chore: update food

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+
 import classes from "./page.module.css";
 
 export default function Home() {
@@ -8,16 +9,42 @@ export default function Home() {
         <div className={classes.slideshow}></div>
         <div>
           <div className={classes.hero}>
-            <p>NextLevel foods for NextLevel Foodies</p>
+            <h1>NextLevel Food for NextLevel Foodies</h1>
+            <p>Taste & share food from all over the world.</p>
           </div>
           <div className={classes.cta}>
-            <Link href="/community"> Join out community </Link>
-            <Link href="/meals"> Explore meal </Link>
+            <Link href="/community">Join the Community</Link>
+            <Link href="/meals">Explore Meals</Link>
           </div>
         </div>
       </header>
+      <main>
+        <section className={classes.section}>
+          <h2>How it works</h2>
+          <p>
+            NextLevel Food is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes,
+            and to connect with other food lovers.
+          </p>
+          <p>
+            NextLevel Food is a place to discover new dishes, and to connect
+            with other food lovers.
+          </p>
+        </section>
 
-      <main></main>
+        <section className={classes.section}>
+          <h2>Why NextLevel Food?</h2>
+          <p>
+            NextLevel Food is a platform for foodies to share their favorite
+            recipes with the world. It&apos;s a place to discover new dishes,
+            and to connect with other food lovers.
+          </p>
+          <p>
+            NextLevel Food is a place to discover new dishes, and to connect
+            with other food lovers.
+          </p>
+        </section>
+      </main>
     </>
   );
 }
