f88eebf0a67143f9d4a4a1a9a2c711aebfc84535
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-02 06:35:36 +0100

    chore: update app

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import classes from "./page.module.css";
 
 export default function Home() {
@@ -6,8 +7,13 @@ export default function Home() {
       <header className={classes.header}>
         <div className={classes.slideshow}></div>
         <div>
-          <div className={classes.hero}></div>
-          <div className={classes.cta}></div>
+          <div className={classes.hero}>
+            <p>NextLevel foods for NextLevel Foodies</p>
+          </div>
+          <div className={classes.cta}>
+            <Link href="/community"> Join out community </Link>
+            <Link href="/meals"> Explore meal </Link>
+          </div>
         </div>
       </header>
 
