e8aac8ff7c25a9513ded676007e0d0e86d29de48
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-02 07:06:40 +0100

    chore: upddate community file

--- components/NavLink/NavLink.jsx
+++ components/NavLink/NavLink.jsx
@@ -1,11 +1,22 @@
 "use client";
+import classes from "./NavLink.module.css";
 import Link from "next/link";
 import { useParams } from "next/navigation";
 
 function NavLink({ children, to }) {
   const params = useParams();
 
-  return <Link></Link>;
+  return (
+    <Link
+      className={
+        params.startsWith(to)
+          ? `${classes.active} ${classes.link}`
+          : classes.link
+      }
+    >
+      {children}
+    </Link>
+  );
 }
 
 export default NavLink;
