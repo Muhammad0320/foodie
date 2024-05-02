18e9a35b9e7dba53b097ad561bf7f08387a0a666
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-02 10:04:12 +0100

    chore:  update page

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,8 +1,23 @@
+import Link from "next/link";
+import classes from "./page.module.css";
+
 function Meals() {
   return (
-    <div>
-      <main>This is the meals page</main>
-    </div>
+    <>
+      <header className={classes.header}>
+        <h1>
+          {" "}
+          Delicious meals created,{" "}
+          <span className={classes.highlight}> by you </span>{" "}
+        </h1>
+        <p>Choose your favorite recipe, its easy and fun!</p>
+        <p className={classes.cts}>
+          <Link href="/meals/share"> share your favorite recipe </Link>
+        </p>
+      </header>
+
+      <main></main>
+    </>
   );
 }
 
