d38971409308c59f1303ae5309da3f43eef0a526
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-02 06:26:54 +0100

    chore: update

--- components/main-header/mainHeader.jsx
+++ components/main-header/mainHeader.jsx
@@ -2,26 +2,30 @@ import Link from "next/link";
 import classes from "./mainHeader.module.css";
 import logo from "@/assets/logo.png";
 import Image from "next/image";
+import MainHeaderBackground from "./mainHeaderBackground";
 
 function MainHeader() {
   return (
-    <header className={classes.header}>
-      <Link href="/" className={classes.logo}>
-        <Image src={logo} alt="A plate w/ food init" priority />
-        NextLevel foods
-      </Link>
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link href="/" className={classes.logo}>
+          <Image src={logo} alt="A plate w/ food init" priority />
+          NextLevel foods
+        </Link>
 
-      <nav className={classes.nav}>
-        <ul>
-          <li>
-            <Link href="/community"> Meals community </Link>
-          </li>
-          <li>
-            <Link href="meals"> Browse Meals </Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/community"> Meals community </Link>
+            </li>
+            <li>
+              <Link href="meals"> Browse Meals </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
 
