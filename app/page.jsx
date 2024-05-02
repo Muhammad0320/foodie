e2f47cd358b0527b4ecf6545a11ab2baa18eb799
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-02 06:33:27 +0100

    chore:  update page

---
+++ app/page.jsx
@@ -0,0 +1,17 @@
+import classes from "./page.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.slideshow}></div>
+        <div>
+          <div className={classes.hero}></div>
+          <div className={classes.cta}></div>
+        </div>
+      </header>
+
+      <main></main>
+    </>
+  );
+}
