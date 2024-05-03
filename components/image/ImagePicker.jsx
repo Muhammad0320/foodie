789e8fcb826ad01a725dee500db079ac14952778
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-03 07:06:53 +0100

    chore: update image picker

--- components/image/ImagePicker.jsx
+++ components/image/ImagePicker.jsx
@@ -1,5 +1,34 @@
-function ImagePicker() {
-  return <div></div>;
+"use client";
+
+import { useRef } from "react";
+import classes from "./imagePicker.module.css";
+
+function ImagePicker({ name, label }) {
+  const inputRef = useRef();
+
+  const handlePickImage = () => {
+    inputRef.current.click();
+  };
+
+  return (
+    <div className={classes.picker}>
+      <label htmlFor={name}> {label} </label>
+
+      <div className={classes.controls}>
+        <input
+          type="file"
+          name={name}
+          id={name}
+          className={classes.input}
+          ref={inputRef}
+        />
+
+        <button className={classes.button} onClick={handlePickImage}>
+          Pick an image
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default ImagePicker;
