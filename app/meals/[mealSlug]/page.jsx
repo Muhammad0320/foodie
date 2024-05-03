fc08c505473b6982bf14c9f0c7e0ce40b35dbdfd
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-03 05:24:19 +0100

    chore: update page jsx

--- app/meals/[mealSlug]/page.jsx
+++ app/meals/[mealSlug]/page.jsx
@@ -1,5 +1,31 @@
+import Image from "next/image";
+import classes from "./page.module.css";
+
 function MealDetails() {
-  return <div>This is the meals details page</div>;
+  return (
+    <>
+      <header className={classes.header}>
+        <Image fill />
+
+        <div className={classes.headerText}>
+          <h1>Title</h1>
+
+          <p className={classes.creator}>
+            by <a href="meailto:name">Name</a>
+          </p>
+
+          <p className={classes.summary}>Summary</p>
+        </div>
+      </header>
+
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: "..." }}
+        ></p>
+      </main>
+    </>
+  );
 }
 
 export default MealDetails;
