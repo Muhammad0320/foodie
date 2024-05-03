7fbe3428c41a1ab9cd990d2798daa9bf315194b0
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-05-03 07:17:54 +0100

    chore:  update image pciker

--- components/image/ImagePicker.jsx
+++ components/image/ImagePicker.jsx
@@ -1,13 +1,32 @@
 "use client";
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import classes from "./imagePicker.module.css";
 
 function ImagePicker({ name, label }) {
   const inputRef = useRef();
 
+  const [pickImage, setPickedImage] = useState();
+
   const handlePickImage = () => {
     inputRef.current.click();
+  };
+
+  const handleOnChangeImage = (event) => {
+    const file = event.target.files[0];
+
+    if (!file) {
+      setPickedImage(null);
+      return;
+    }
+
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      setPickedImage(reader.result);
+    };
+
+    reader.readAsDataURL(file);
   };
 
   return (
@@ -21,6 +40,8 @@ function ImagePicker({ name, label }) {
           id={name}
           className={classes.input}
           ref={inputRef}
+          onChange={handleOnChangeImage}
+          required
         />
 
         <button className={classes.button} onClick={handlePickImage}>
